ae6e8de4d750d5dfd22d0f1f6ce1aaaa4e683d1c
Josué Arrieta Salas <josuearrietasalas@gmail.com>
2016-08-19 21:36:06 -0600

    JUnit Ready

--- WebInterface/src/Logic/Testing/ImageProcessorTest.java
+++ WebInterface/src/Logic/Testing/ImageProcessorTest.java
@@ -23,41 +23,41 @@ public class ImageProcessorTest {
 	}
 	@Test
 	public void testRgb2hsv() {
-		assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("2-hsv.png"), ImageProcessor.rgb2hsv(Imgcodecs.imread("1-rgb.png"))));
+		assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("testData/2-hsv.png"), ImageProcessor.rgb2hsv(Imgcodecs.imread("testData/1-rgb.png"))));
 	}
 	@Test
 	public void testGreenMask() {
-		 assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("3-binaria.png"), ImageProcessor.greenMask(Imgcodecs.imread("2-hsv.png"))));
+		 assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("testData/3-binaria.png"), ImageProcessor.greenMask(Imgcodecs.imread("testData/2-hsv.png"))));
 	}
 	@Test
 	public void testDilate() {
-		 assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("4-dilatada.png"), ImageProcessor.dilate(Imgcodecs.imread("3-binaria.png"))));
+		 assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("testData/4-dilatada.png"), ImageProcessor.dilate(Imgcodecs.imread("testData/3-binaria.png"))));
 	}
 	@Test
 	public void testImfill() {
-		assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("5-rellenada.png"), ImageProcessor.imfill(Imgcodecs.imread("4-dilatada.png"))));
+		assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("testData/5-rellenada.png"), ImageProcessor.imfill(Imgcodecs.imread("testData/4-dilatada.png"))));
 	}
 	@Test
 	public void testBwareaopen() {
-	    Mat realImage = Imgcodecs.imread("5-rellenada.png");
+	    Mat realImage = Imgcodecs.imread("testData/5-rellenada.png");
 	    Mat grayRealImage = new Mat();
         Imgproc.cvtColor(realImage, grayRealImage, Imgproc.COLOR_RGB2GRAY);
-		assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("6-pulida.png"), ImageProcessor.bwareaopen(grayRealImage)));
+		assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("testData/6-pulida.png"), ImageProcessor.bwareaopen(grayRealImage)));
 	}
 	@Test
 	public void testFinalTouch() {
-		Mat realImage = Imgcodecs.imread("6-pulida.png");
+		Mat realImage = Imgcodecs.imread("testData/6-pulida.png");
 	    Mat grayRealImage = new Mat();
         Imgproc.cvtColor(realImage, grayRealImage, Imgproc.COLOR_RGB2GRAY);
-        assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("7-finalTouch.png"), ImageProcessor.finalTouch(grayRealImage)));
+        assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("testData/7-finalTouch.png"), ImageProcessor.finalTouch(grayRealImage)));
 	}
 	@Test
 	public void testRemoveScore() {
-		assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("8-noScore.png"), ImageProcessor.removeScore(Imgcodecs.imread("7-finalTouch.png"))));
+		assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("testData/8-noScore.png"), ImageProcessor.removeScore(Imgcodecs.imread("testData/7-finalTouch.png"))));
 	}
 	@Test
 	public void testDetectField(){
-		assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("8-noScore.png"), ImageProcessor.detectField(Imgcodecs.imread("1-rgb.png"))));
+		assertTrue(ImageProcessor.compareImage(Imgcodecs.imread("testData/8-noScore.png"), ImageProcessor.detectField(Imgcodecs.imread("testData/1-rgb.png"))));
 	}
 
 }
