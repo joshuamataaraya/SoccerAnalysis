5797ecaffbca0a3c8f069ee93d2c7f3dff6883a0
xelop <adlopez@ic-itcr.ac.cr>
2016-10-06 15:17:31 -0600

    File Upload/Loading circlet

--- Implementation/Source Code/BocaAnalyticsNuevo/src/main/webapp/js/consumable.js
+++ Implementation/Source Code/BocaAnalyticsNuevo/src/main/webapp/js/consumable.js
@@ -1,7 +1,9 @@
 var app = angular.module('myApp',[]);
 app.controller('myCtrl', function($scope) {
-	
-   	$scope.connect = function() {
+   
+    var webSocket;
+
+	$scope.connect = function() {
 		// oprn the connection if one does not exist
 		if (webSocket !== undefined
 				&& webSocket.readyState !== WebSocket.CLOSED) {
@@ -11,6 +13,7 @@ app.controller('myCtrl', function($scope) {
 		webSocket = new WebSocket("ws://localhost:8080/toUpper");
 
 		webSocket.onopen = function(event) {
+		    
 			$scope.updateOutput("Connected!");
 			connectBtn.disabled = true;
 			sendBtn.disabled = false;
@@ -19,6 +22,9 @@ app.controller('myCtrl', function($scope) {
 
 		webSocket.onmessage = function(event) {
 			$scope.updateOutput(event.data);
+			$scope.loading = false;
+			$scope.$digest();
+			console.log($scope.loading);
 		};
 
 		webSocket.onclose = function(event) {
@@ -29,8 +35,26 @@ app.controller('myCtrl', function($scope) {
 	}
 
 	$scope.send = function(){
-		var text = document.getElementById("input").value;
-		webSocket.send(text);
+		console.log("here");
+		$scope.loading = true;
+		console.log($scope.loading);
+        var file = document.getElementById('filename').files[0];
+        webSocket.send('filename:'+file.name);
+        var reader = new FileReader();
+        var rawData = new ArrayBuffer();            
+        //alert(file.name);
+
+        reader.loadend = function() {
+
+        }
+        reader.onload = function(e) {
+        	$scope.loading = true;
+            rawData = e.target.result;
+            webSocket.send(rawData);
+            webSocket.send('end');
+        }
+
+        reader.readAsArrayBuffer(file);
 	}
 
 	$scope.closeSocket = function() {
@@ -40,5 +64,50 @@ app.controller('myCtrl', function($scope) {
 	$scope.updateOutput = function(text) {
 		output.innerHTML += "<br/>" + text;
 	}
+
+    function connectChatServer() {
+        webSocket = new WebSocket(
+                "ws://localhost:8080/toUpper");
+
+        webSocket.binaryType = "arraybuffer";
+        webSocket.onopen = function() {
+            alert("Connected.")
+        };
+
+        webSocket.onmessage = function(evt) {
+            alert(evt.msg);
+        };
+
+        webSocket.onclose = function() {
+            alert("Connection is closed...");
+        };
+        webSocket.onerror = function(e) {
+            alert(e.msg);
+        }
+
+    }
+
+    $scope.sendFile=function(){
+    	console.log("here");
+    	connectChatServer();
+        var file = document.getElementById('filename').files[0];
+        webSocket.send('filename:'+file.name);
+        var reader = new FileReader();
+        var rawData = new ArrayBuffer(); 
+        $scope.loading = true;
+        //alert(file.name);
+
+        reader.loadend = function() {
+
+        }
+        reader.onload = function(e) {
+            rawData = e.target.result;
+            webSocket.send(rawData);
+            webSocket.send('end');
+        }
+
+        reader.readAsArrayBuffer(file);
+
+    }
 	
 });
