a258840037f6ccdd803dfad95ea82e0900e71289
Joshua Mata <joshuamataa@gmail.com>
2016-10-09 22:04:19 -0600

    Divide in frames
    + Tests for frames count
    + Divide video in frames with opencv method implemented

--- Implementation/Source Code/BocaAnalyticsNuevo/src/main/java/logic/videoprocessor/OpenCVVideoProcessor.java
+++ Implementation/Source Code/BocaAnalyticsNuevo/src/main/java/logic/videoprocessor/OpenCVVideoProcessor.java
@@ -6,34 +6,53 @@ package logic.videoprocessor;
 import java.util.ArrayList;
 
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
 import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.Videoio;
 
 public class OpenCVVideoProcessor extends VideoProcessor {
-
+	
+	private int countFrames;
+	private VideoCapture video;
 	public OpenCVVideoProcessor(String filePath) {
 		super(filePath);
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);	
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 	}
 	
-
-	@Override
 	public ArrayList<Object> getVideoFrames() {
-		ArrayList frames = new ArrayList<>();
-		VideoCapture videoCapture = new VideoCapture(filePath);
-		return null;
+		ArrayList<Object> frames = new ArrayList<>();
+		openVideo();
+		Mat currentMat =  new Mat();
+		while (video.read(currentMat)){
+			Mat newMat = new Mat();
+			newMat = currentMat;
+			frames.add(newMat);
+			countFrames++;
+		}
+		return frames;
 	}
-	
 
 	@Override
 	public int countFrames() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (countFrames==0){
+			Mat mat = new Mat();
+			openVideo();
+			while (video.read(mat)){
+				countFrames++;
+			}			
+		}
+		return countFrames;
 	}
 
 	@Override
 	public String createVideo(ArrayList<Object> frames) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	private void openVideo (){
+		System.loadLibrary("opencv_ffmpeg310_64");
+		video = new VideoCapture(filePath);
 	}
 
 }
--- Implementation/Source Code/BocaAnalyticsNuevo/src/main/java/logic/videoprocessor/OpenCVVideoProcessorTest.java
+++ Implementation/Source Code/BocaAnalyticsNuevo/src/main/java/logic/videoprocessor/OpenCVVideoProcessorTest.java
@@ -11,10 +11,19 @@ import org.opencv.core.Core;
 public class OpenCVVideoProcessorTest {
 
 	@Test
-	public void test() {
-
+	public void countFramesTest1() {
 		OpenCVVideoProcessor vp = new OpenCVVideoProcessor("testData/video.mp4");
-		Object o = vp.getVideoFrames();
+		int expected = 132;
+		int actual = vp.countFrames();
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void countFramesTest2() {
+		OpenCVVideoProcessor vp = new OpenCVVideoProcessor("testData/video.mp4");
+		vp.getVideoFrames();
+		int expected = 132;
+		int actual = vp.countFrames();
+		assertEquals(expected, actual);
 	}
 
 }
