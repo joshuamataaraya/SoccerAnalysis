4519dd4cc0508e2b88aec128080f921923339106
Joshua Mata <joshuamataa@gmail.com>
2016-10-12 20:19:36 -0600

    Test Ground Truth

--- Implementation/Source Code/BocaAnalyticsNuevo/src/main/java/controller/groundTruthControllerTest.java
+++ Implementation/Source Code/BocaAnalyticsNuevo/src/main/java/controller/groundTruthControllerTest.java
@@ -4,11 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import DataTransferObject.DTOGroundTruth;
+import DataTransferObject.DTOVideoAnalisis;
+
 public class groundTruthControllerTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGroundTruth() throws Exception {
+		DTOGroundTruth dto = new DTOGroundTruth();
+		dto.setVideoPath("testData/1476324147909.mp4");
+		dto.setGrundVideoPath("testData/1476324147909.mp4");
+		
+		GroundTruthController gt = new GroundTruthController();
+		gt.algoritm(dto);
 	}
 
 }
