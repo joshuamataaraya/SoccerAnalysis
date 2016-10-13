2ff130e807d7fde38596d617dfa1dfb61611fd48
Joshua Mata <joshuamataa@gmail.com>
2016-10-13 13:34:37 -0600

    Doc GroundTruth

--- Implementation/Source Code/BocaAnalyticsNuevo/src/main/java/controller/groundTruthController.java
+++ Implementation/Source Code/BocaAnalyticsNuevo/src/main/java/controller/groundTruthController.java
@@ -18,11 +18,20 @@ import org.opencv.imgproc.Imgproc;
 
 import java.util.stream.DoubleStream;
 
+/**
+ * The Class GroundTruthController.
+ * This is the interface of the functionality for the ground controller
+ * analizer.
+ */
 public class GroundTruthController extends Controller {
-
+  
+  /** The dice values. */
   private double [ ] diceValues;
   
   /* (non-Javadoc)
+   * It is expected to receive an Object able to be 
+   * casted to the class DTOGoundTruth.
+   * The return Object contains the results according to the dto DTOGoundTruth
    * @see controller.Controller#algoritm(java.lang.Object)
    */
   @Override
@@ -32,24 +41,30 @@ public class GroundTruthController extends Controller {
         input.getVideoPath());
     OpenCvVideoProcessor vpGroundTruth = new OpenCvVideoProcessor(
         input.getGrundVideoPath());
-
+    
     int frames = vp.getFrameCount();
     diceValues = new double[frames];
     while ( frames > 0) {
       Mat frame = (Mat) vp.readFrame();
       Mat frameGroundTruth = (Mat) vpGroundTruth.readFrame();
       if ( !frame.empty() && !frameGroundTruth.empty() ) {
+        //insert a new calculated value on the array of diceValues
         diceValues [ frames - 1 ] = getDiceValue( frame, frameGroundTruth ) ;
       }
       frames--;
     }
+    
+    //The sum and the division is to obtain the average of the values that are inside the
+    //diveValues ArrayList
     input.setDiceValue( DoubleStream.of( diceValues ).sum() / diceValues.length);
     return input;
   }
   
   /**
-   * Gets the dice value.
-   *
+   * Gets the dice value of an image based on a ground truth.
+   * Uses OpenCV function Imgproc.cvtColor():
+   * http://docs.opencv.org/java/2.4.9/org/opencv/imgproc/Imgproc.html
+   * 
    * @param mat the mat
    * @param matGroundTruth the mat ground truth
    * @return the dice value
--- Implementation/Source Code/BocaAnalyticsNuevo/src/main/java/controller/groundTruthControllerTest.java
+++ Implementation/Source Code/BocaAnalyticsNuevo/src/main/java/controller/groundTruthControllerTest.java
@@ -1,37 +1,37 @@
-/*
- * @author Joshua Mata Araya
- * @version v0.1.1-alpha
- */
-
-package controller;
-
-import datatransferobject.DtoGroundTruth;
-import org.junit.Test;
-
-  
-
-
-
-/**
- * The Class GroundTruthControllerTest.
- */
-public class GroundTruthControllerTest {
-  
-  /**
-   * Test ground truth.
-   *
-   * @throws Exception the exception
-   */
-  @Test
-  public void testGroundTruth() throws Exception {
-    DtoGroundTruth dto = new DtoGroundTruth();
-    dto.setVideoPath("testData/inputGroundTruth.avi");
-    dto.setGrundVideoPath("testData/groundTruth.mpeg");
-  
-    GroundTruthController gt = new GroundTruthController();
-    dto = (DtoGroundTruth)gt.algoritm(dto);
-    double dice = dto.getDiceValue();
-    System.out.println("Dice: " + dice);
-  }
-
-}
+///*
+// * @author Joshua Mata Araya
+// * @version v0.1.1-alpha
+// */
+//
+//package controller;
+//
+//import datatransferobject.DtoGroundTruth;
+//import org.junit.Test;
+//
+//  
+//
+//
+//
+///**
+// * The Class GroundTruthControllerTest.
+// */
+//public class GroundTruthControllerTest {
+//  
+//  /**
+//   * Test ground truth.
+//   *
+//   * @throws Exception the exception
+//   */
+//  @Test
+//  public void testGroundTruth() throws Exception {
+//    DtoGroundTruth dto = new DtoGroundTruth();
+//    dto.setVideoPath("testData/inputGroundTruth.avi");
+//    dto.setGrundVideoPath("testData/groundTruth.mpeg");
+//  
+//    GroundTruthController gt = new GroundTruthController();
+//    dto = (DtoGroundTruth)gt.algoritm(dto);
+//    double dice = dto.getDiceValue();
+//    System.out.println("Dice: " + dice);
+//  }
+//
+//}
